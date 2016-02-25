f6fb2aa75544e9546a0e7db4ffa57af55f4bbf28
Mike Giordano <michael.giordano@criticalmedia.com>
2016-02-24 19:03:31 -0500

    Started Text/reverse_string.py

---
+++ Text/reverse_string.py
@@ -0,0 +1,13 @@
+"""
+Reverse a String - Enter a string and the program will reverse it and print it out.
+"""
+#does not yet work
+
+
+def reverse_string(user_input):
+	print user_input.split()
+	pass
+
+while True:
+	indo = raw_input("What do you need reversed? \n -->")
+	reverse_string(indo)
