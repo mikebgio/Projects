fdb2cda4b4c077cfb366feb2c676a5eaeb2cfd7b
mikebgio <mgiord1987@gmail.com>
2016-02-27 19:43:36 -0500

    Add Text/word_counter.py and companion file. Needs Work

---
+++ Text/word_counter.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+"""
+Count Words in a String - Counts the number of individual words 
+in a string. For added complexity read these strings 
+in from a text file and generate a summary.
+"""
+
+#Currently has an issue where it's adding empty strings to the split list
+#Has something to do with instances of '\n\n'
+#look into dividing separate words in a better way
+
+def main():
+	indo = raw_input("Enter file path: ")
+	c = count_words(indo)
+	print "There are %d words in '%s'" %(c, indo)
+
+def count_words(file):
+	with open(file, 'r') as f:
+		read_data = f.read()
+		stripped = read_data.replace('\n',' ')
+		words = stripped.split(' ')
+		print words
+		return len(words)
+
+if __name__ == '__main__':main()
