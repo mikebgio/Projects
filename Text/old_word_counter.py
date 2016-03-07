c522137935cfcc5321ff4ba03c90d646250827b7
Mike Giordano <michael.giordano@criticalmedia.com>
2016-03-07 18:35:38 -0500

    Swapped word_counter versions and removed old comments

--- Text/old_word_counter.py
+++ Text/old_word_counter.py
@@ -1,27 +1,26 @@
 #!/usr/bin/python
+#THIS ONE IS OLD AND LAME
 """
 Count Words in a String - Counts the number of individual words 
 in a string. For added complexity read these strings 
 in from a text file and generate a summary.
 """
-import sys
+
 #Currently has an issue where it's adding empty strings to the split list
 #Has something to do with instances of '\n\n'
 #look into dividing separate words in a better way
 
-def main(argv):
-	words = split_words(argv)
-	print "There are %d words in '%s'" %(len(words), argv)
-	print "There are %d unique words in '%s'" %(len(set(words)), argv)
+def main():
+	indo = raw_input("Enter file path: ")
+	c = count_words(indo)
+	print "There are %d words in '%s'" %(c, indo)
 
-def split_words(file):
+def count_words(file):
 	with open(file, 'r') as f:
 		read_data = f.read()
-		lowcase = read_data.lower()
-		stripped = lowcase.translate(None, ',;."!?[]()')
-		dehyphen = stripped.replace('-',' ')
-		words = dehyphen.split()
-		return words
+		stripped = read_data.replace('-',' ')
+		words = stripped.split()
+		print words
+		return len(words)
 
-
-if __name__ == '__main__':main(sys.argv[1])
+if __name__ == '__main__':main()
--- Text/word_counter.py
+++ Text/word_counter.py
@@ -1,26 +1,24 @@
 #!/usr/bin/python
-#THIS ONE IS OLD AND LAME
 """
 Count Words in a String - Counts the number of individual words 
 in a string. For added complexity read these strings 
 in from a text file and generate a summary.
 """
+import sys
 
-#Currently has an issue where it's adding empty strings to the split list
-#Has something to do with instances of '\n\n'
-#look into dividing separate words in a better way
+def main(argv):
+	words = split_words(argv)
+	print "There are %d words in '%s'" %(len(words), argv)
+	print "There are %d unique words in '%s'" %(len(set(words)), argv)
 
-def main():
-	indo = raw_input("Enter file path: ")
-	c = count_words(indo)
-	print "There are %d words in '%s'" %(c, indo)
-
-def count_words(file):
+def split_words(file):
 	with open(file, 'r') as f:
 		read_data = f.read()
-		stripped = read_data.replace('-',' ')
-		words = stripped.split()
-		print words
-		return len(words)
+		lowcase = read_data.lower()
+		stripped = lowcase.translate(None, ',;."!?[]()')
+		dehyphen = stripped.replace('-',' ')
+		words = dehyphen.split()
+		return words
 
-if __name__ == '__main__':main()
+
+if __name__ == '__main__':main(sys.argv[1])
